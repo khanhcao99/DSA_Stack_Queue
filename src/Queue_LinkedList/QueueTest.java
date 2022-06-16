940abc3b00ea9692ca58d90aab6fff529d154a0f
khanhcao99 <khanhboy1324@gmail.com>
2022-06-16 23:20:32 +0700

    Queue_LinkedList

---
+++ src/Queue_LinkedList/QueueTest.java
@@ -0,0 +1,30 @@
+//package Queue_LinkedList;
+//
+//public class QueueTest {
+//    public static void main(String[] args) {
+//        // Create a queue and initialize front and rear
+//        Queue q = new Queue();
+//        q = q.re =  null;
+//
+//        // Inserting elements in Circular Queue
+//
+//        enQueue(q, 22);
+//        enQueue(q, 6);
+//
+//        // Display elements present in Circular Queue
+//        displayQueue(q);
+//
+//        // Deleting elements from Circular Queue
+//        System.out.printf("
+//                Deleted value = %d", deQueue(q));
+//        System.out.printf("
+//                Deleted value = %d", deQueue(q));
+//
+//        // Remaining elements in Circular Queue
+//        displayQueue(q);
+//
+//        enQueue(q, 9);
+//        enQueue(q, 20);
+//        displayQueue(q);
+//    }
+//}
